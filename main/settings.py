dfae5940a5c8af678583c77c59a5cca1385ef12a
huseyintekin <huseyintekin10@gmail.com>
2023-08-03 12:11:15 +0200

    qs

--- main/settings.py
+++ main/settings.py
@@ -139,4 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAdminUser'],
 }
--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,30 @@
-# from rest_framework import routers
+from django.urls import path
+
+# from rest_framework import routers
 from rest_framework.routers import DefaultRouter
-from .views import
+from .views import UserView
+
+# ------------------------------------------------
+# Login:
+from rest_framework.authtoken.views import obtain_auth_token
+# ------------------------------------------------
+# Logout:
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+@api_view(['POST'])
+def logout(request):
+    request.user.auth.token.delete() # Token Sil.
+    return Response({"message": 'User Logout: Token Deleted'})
+# ------------------------------------------------
+# Login/Logout
+urlpatterns = [
+    path('login/', obtain_auth_token),
+    path('logout/', logout),
+]
+# ------------------------------------------------
 
 # ROUTER:
 router = DefaultRouter()
+router.register('', UserView)
 
-urlpatterns = router.urls
+urlpatterns += router.urls
